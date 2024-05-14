73706e686a63d83c09e2ca237fe4d107771fcdb3
matheus <matheus.pmc@hotmail.com>
2024-05-14 19:42:51 -0300

    Iniciando a tela Principal

---
+++ Backend-Brafe/src/main/java/com/Brafe/Brafe/BrafeApplication.java
@@ -0,0 +1,13 @@
+package com.Brafe.Brafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrafeApplication.class, args);
+	}
+
+}
