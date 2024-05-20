d31146fda54df1dd50e8b3f01d7f86a110498e71
matheus <matheus.pmc@hotmail.com>
2024-05-20 20:44:51 -0300

    identacao, chamadas e tratativa de erro login pronto

--- Backend-Brafe/src/main/java/com/Brafe/Brafe/adapter/in/controller/UsuarioController.java
+++ Backend-Brafe/src/main/java/com/Brafe/Brafe/adapter/in/controller/UsuarioController.java
@@ -7,23 +7,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("login")
+@RequestMapping("usuario")
 public class UsuarioController {
 
-    @PostMapping
+    private Usuario usuario;
+
+    @PostMapping("/login")
     @CrossOrigin(origins = "http://localhost:8080")
     public ResponseEntity<Usuario> login(@RequestBody Login loginRequest) {
-
         System.out.println(loginRequest);
-        if (loginRequest.getEmail().equalsIgnoreCase("matheus@test.com.br")) {
-            Usuario usuario = new Usuario();
-            usuario.setId(1L);
-            usuario.setTestando("testando");
-            usuario.setUsername("Matheus");
-            usuario.setTestando2("testando2");
-            System.out.println("entrou aq");
+        if (usuario != null && loginRequest.getEmail().equalsIgnoreCase(usuario.getEmail()) && loginRequest.getPassword().equals(usuario.getPassword())) {
             return ResponseEntity.status(HttpStatus.OK).body(usuario);
         } else
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+
+    @PostMapping("/cadastro")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public ResponseEntity<Usuario> cadastro(@RequestBody Usuario cadastroRequest) {
+        System.out.println(cadastroRequest);
+        if (usuario == null)
+            usuario = new Usuario();
+        if (usuario.getId() != null)
+            cadastroRequest.setId(usuario.getId() + 1L);
+        else
+            cadastroRequest.setId(1L);
+
+        usuario = cadastroRequest;
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
+    @PutMapping("/atualizar")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public ResponseEntity<Usuario> atualizar(@RequestBody Usuario cadastroRequest) {
+        System.out.println(cadastroRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(cadastroRequest);
     }
 }
--- Backend-Brafe/src/main/java/com/Brafe/Brafe/adapter/in/model/Usuario.java
+++ Backend-Brafe/src/main/java/com/Brafe/Brafe/adapter/in/model/Usuario.java
@@ -12,9 +12,14 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Usuario {
     private Long id;
-    private String username;
-    private String testando;
-    private String testando2;
+    private String email;
+    private String password;
+    private String nomeCompleto;
+    private String cpf;
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cep;
 
     public Long getId() {
         return id;
@@ -24,27 +29,82 @@ public class Usuario {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public String getTestando() {
-        return testando;
+    public String getPassword() {
+        return password;
     }
 
-    public void setTestando(String testando) {
-        this.testando = testando;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getTestando2() {
-        return testando2;
+    public String getNomeCompleto() {
+        return nomeCompleto;
     }
 
-    public void setTestando2(String testando2) {
-        this.testando2 = testando2;
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", nomeCompleto='" + nomeCompleto + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", rua='" + rua + '\'' +
+                ", numero='" + numero + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cep='" + cep + '\'' +
+                '}';
     }
 }
